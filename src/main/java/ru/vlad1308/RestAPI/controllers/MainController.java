b0e0a276d58f8964aacc25fdb2864b274fd77447
vlad-1308 <vskrasnik@yandex.ru>
2022-01-21 00:02:07 +0300

    Add template index and controller maincontroller

---
+++ src/main/java/ru/vlad1308/RestAPI/controllers/MainController.java
@@ -0,0 +1,16 @@
+package ru.vlad1308.RestAPI.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/greeting")
+    public String home(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "index";
+    }
+
+}
