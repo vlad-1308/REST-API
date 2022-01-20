dd6beed201c4ac15c8bf8c6565187d47a36a785a
vlad-1308 <vskrasnik@yandex.ru>
2022-01-20 23:10:25 +0300

    vlad-1308
    Add springboot project

---
+++ src/test/java/ru/vlad1308/REST/API/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vlad1308.REST.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
