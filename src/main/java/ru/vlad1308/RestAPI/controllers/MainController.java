93dc12408183da65299b386d0fa95edf4cb014a7
vlad-1308 <vskrasnik@yandex.ru>
2022-01-21 00:20:05 +0300

    Fix controller MainController

--- src/main/java/ru/vlad1308/RestAPI/controllers/MainController.java
+++ src/main/java/ru/vlad1308/RestAPI/controllers/MainController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/greeting")
-    public String home(Model model) {
-        model.addAttribute("title", "Главная страница");
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("name", "Главная страница");
         return "index";
     }
 
